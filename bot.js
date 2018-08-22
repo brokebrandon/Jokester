51eb573057fb056861c8e450a48ef6f0aa51040a
brokebrandon <norcalnbk@gmail.com>
2018-08-21 23:14:46 -0500

    botconfig.json -> config.json - structure of file changed

--- bot.js
+++ bot.js
@@ -1,32 +1,57 @@
-const botconfig = require('./botconfig.json');
+// Initialize JSON object containing private things
+const config = require('./config.json');
+
+// Set up Discord client
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-//Get the Jokester ready for action
+// Set up connection to jokes database
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(`./${config.database.name}`, err => {
+	if(err) {
+		return console.log(err.message);
+	}
+	console.log(`Connected to ${config.database.name}`);
+
+});
+
+// Get the Jokester ready for action
 client.on('ready', () => {
 	console.log(`${client.user.username} is now online.`);
 	console.log(`Serving ${client.users.size} users.`);
-
 	client.user.setActivity('Joke Simulator');
 
 });
 
-//React to messages
+// React to messages
 client.on('message', async message => {
-	//if a bot triggers Jokester, ignore it
+	// if a bot triggers Jokester, ignore it
 	if(message.author.bot) return;
-	//if message doesn't start with prefix, ignore it
-	if(message.content.indexOf(botconfig.prefix) !== 0) return;
+	// if message doesn't start with prefix, ignore it
+	if(message.content.indexOf(config.bot.prefix) !== 0) return;
 
 	let messageArray = message.content.toLowerCase().split(' ');
 	const command = messageArray[0];
 	const args = messageArray.slice(1);
 
-	if(command === `${botconfig.prefix}ping`) {
-		const m = await message.channel.send('Calculating ping...');
-		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
+	switch (command) {
+		case `${config.bot.prefix}ping`:
+			const m = await message.channel.send('Calculating ping...');
+			m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
+			break;
+		case `${config.bot.prefix}tellmea`:
+			console.log(args[0]);
+			let joke = getJoke(args[0]);
+			message.channel.send(joke);
+			break;
+		default:
+			break;
 	}
 
 });
 
-client.login(botconfig.token);
+function getJoke(category){
+	// TODO: get joke from database
+}
+
+client.login(config.bot.token);
