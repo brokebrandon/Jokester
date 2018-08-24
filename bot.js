c0b6d7fa03e43c354a353dbb311de9006bfcfeba
brokebrandon <norcalnbk@gmail.com>
2018-08-23 22:16:46 -0500

    added joke rating system

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ const client = new Discord.Client();
 const db = require('sqlite');
 db.open(`./${config.db.name}`);
 
+//rateReminder appended at end of some messages
+var rateReminder;
+
+
 // Get the Jokester ready for action
 client.on('ready', () => {
 	console.log(`${client.user.username} is now online.`);
@@ -16,6 +20,7 @@ client.on('ready', () => {
 	client.user.setActivity('Joke Simulator');
 
 });
+
 
 // React to messages
 client.on('message', async message => {
@@ -32,18 +37,90 @@ client.on('message', async message => {
 		case `${config.bot.prefix}ping`:
 			const m = await message.channel.send('Calculating ping...');
 			m.delete();
-
 			message.reply(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
+
 			break;
 		case `${config.bot.prefix}tellmea`:
-			db.all(`SELECT * FROM jokes where category='${args[0]}'`)
+			if(args[0] != 'joke') {
+				db.all(`SELECT * FROM jokes where category='${args[0]}'`)
 				.then(row => {
-					let id = Math.floor(Math.random() * (row.length - 1 + 1));
-					message.reply(row[id].joke);
+					let joke_id = Math.floor(Math.random() * (row.length - 1 + 1));
+					rateReminder = `\n\n😆 (${row[joke_id].upvotes})  😦 (${row[joke_id].downvotes})`;
+					message.reply(`**${row[joke_id].joke}**${rateReminder}`)
 				})
 				.catch(error => {
 					console.log(error);
 				});
+				break;
+			}
+
+			db.all(`SELECT * FROM jokes`)
+			.then(row => {
+				let joke_id = Math.floor(Math.random() * (row.length - 1 + 1));
+				rateReminder = `\n\n😆 (${row[joke_id].upvotes})  😦 (${row[joke_id].downvotes})`;
+				message.reply(`**${row[joke_id].joke}**${rateReminder}`)
+			})
+			.catch(error => {
+				console.log(error);
+			});
+
+			break;
+		default:
+			break;
+	}
+});
+
+
+client.on('messageReactionAdd', (reaction, user) => {
+	if(reaction.message.author.id != config.bot.id) {
+		return;
+	}
+
+	let jokeArray = reaction.message.content.split('**');
+	let joke = jokeArray[1];
+
+	switch(reaction.emoji.name) {
+		case '😆':
+			sql = `UPDATE jokes SET upvotes = upvotes + 1 WHERE joke="${joke}"`;
+			db.run(sql, error => {
+				console.log(error);
+			});
+
+			sql = `UPDATE user SET upvotes = upvotes + 1 WHERE user_id="${user.id}"`;
+			db.run(sql, error => {
+				console.log(error);
+			});
+
+			db.get(`SELECT * FROM jokes where joke='${joke}'`)
+				.then(row => {
+					rateReminder = `\n\n😆 (${row.upvotes})  😦 (${row.downvotes})`;
+					reaction.message.edit(`**${joke}**${rateReminder}`);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+
+			break;
+		case '😦':
+			sql = `UPDATE jokes SET downvotes = downvotes + 1 WHERE joke="${joke}"`;
+			db.run(sql, error => {
+				console.log(error);
+			});
+
+			sql = `UPDATE user SET downvotes = downvotes + 1 WHERE user_id="${user.id}"`;
+			db.run(sql, error => {
+				console.log(error);
+			});
+
+			db.get(`SELECT * FROM jokes where joke='${joke}'`)
+				.then(row => {
+					rateReminder = `\n\n😆 (${row.upvotes})  😦 (${row.downvotes})`;
+					reaction.message.edit(`**${joke}**${rateReminder}`);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+
 			break;
 		default:
 			break;
@@ -51,11 +128,7 @@ client.on('message', async message => {
 
 });
 
-client.on('messageReactionAdd', reaction => {
-	if(message.author.id == config.bot.id) {
-		// TODO: handle reactions to jokes
-		console.log(reaction);
-	}
-});
+
+
 
 client.login(config.bot.token);
